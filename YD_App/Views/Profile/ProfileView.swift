ef2a0b3197be33f5244fb560cf957779a222c94e
Luis Melendez <luis.nexusstudio.dev@gmail.com>
2025-05-04 16:59:25 -0600

    Proyecto Base

---
+++ YD_App/Views/Profile/ProfileView.swift
@@ -0,0 +1,7 @@
+//
+//  Profile.swift
+//  YD_App
+//
+//  Created by Luis Melendez on 01/05/25.
+//
+
--- YD_App/Views/SplashScreen/SplashScreenView.swift.swift
+++
@@ -1,7 +0,0 @@
-//
-//  SplashScreenView.swift.swift
-//  YD_App
-//
-//  Created by Luis Melendez on 20/03/25.
-//
-
