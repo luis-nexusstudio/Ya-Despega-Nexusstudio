dc30551c337f6bd46a27c80c79704134bbaee700
Pedro <pedro.nexusstudio.dev@gmail.com>
2025-05-12 20:20:22 -0600

    login interno no funcional

---
+++ YD_App/ViewModels/Login/AppDelegate.swift
@@ -0,0 +1,33 @@
+/*
+
+Abstract:
+A struct for accessing generic password keychain items.
+*/
+
+
+import UIKit
+import AuthenticationServices
+import SwiftUI
+
+
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    /// - Tag: did_finish_launching
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let appleIDProvider = ASAuthorizationAppleIDProvider()
+        appleIDProvider.getCredentialState(forUserID: KeychainItem.currentUserIdentifier) { (credentialState, error) in
+            switch credentialState {
+            case .authorized:
+                break // The Apple ID credential is valid.
+            case .revoked, .notFound:
+                // The Apple ID credential is either revoked or was not found, so show the sign-in UI.
+                break
+            default:
+                break
+            }
+        }
+        return true
+    }
+}
